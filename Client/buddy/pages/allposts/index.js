a8f00d407482ce8626d4902858fb9155cd557682
Anirban <anirbansharma36@gmail.com>
2022-12-21 23:38:25 +0530

    added new files

---
+++ Client/buddy/pages/allposts/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Allpost = () => {
+  return (
+    <div>Allpost</div>
+  )
+}
+
+export default Allpost
--- Client/buddy/pages/components/[id].js
+++
@@ -1,8 +0,0 @@
-
-import React from 'react'
-
-export const Abc = () => {
-  return (
-    <div>Abc</div>
-  )
-}
